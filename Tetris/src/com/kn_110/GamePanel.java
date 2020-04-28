e4387ddc03caf7ed323f83a01f33533675a60edd
DavidTelenko <davidtelenko11@gmail.com>
2020-04-28 14:50:20 +0300

    blankCursor
    new bullshit

--- Tetris/src/com/kn_110/GamePanel.java
+++ Tetris/src/com/kn_110/GamePanel.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -58,7 +59,7 @@ public class GamePanel extends JPanel implements ActionListener {
     GamePanel() {
         highScoreLoader();
         highScoreLoader();
-//      blankCursorFeature();
+        blankCursorFeature();
         setOpaque(true);
         setFocusable(true);
         setBackground(new Color(0, 0, 0));
@@ -118,6 +119,12 @@ public class GamePanel extends JPanel implements ActionListener {
                 System.out.println("failed to load audio file");
             }
         }
+    }
+
+    private void blankCursorFeature() {
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+        setCursor(blankCursor);
     }
     //------------------/PRELOAD-------------------
 
